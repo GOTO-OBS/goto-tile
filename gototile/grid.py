bc9e0c544b38125d80675b7b84626f1776a6a16a
Evert Rol <evert.rol@gmail.com>
2015-09-08 11:00:46 +1000

    Move some file naming outside the grid module
    Since the file naming is largely based on user input from options,
    this has been moved to the tileskymap script. This includes the loop
    over the two GOTO configurations.

--- gototile/grid.py
+++ gototile/grid.py
@@ -11,45 +11,26 @@ from . import skymaptools as smt
 import gzip
 import os
 
-def pixelsky(tilesdir,tilelist,scope):
 
-    nside = 256
-    nest = True
+def tileallsky(filename, scopename, nside):
+
+    delns, delew = smt.getdels(scopename)
+
+    north = np.arange(0.0, 90.0, delns)
+    south = -1*north
+    n2s = np.append(south[::-2], north)
+    e2w = np.arange(0.0, 360., delew)
+
+    tilelist = np.array([smt.findFoV(lon, lat, delns, delew) 
+                         for lat, lon in it.product(n2s, e2w)])
+
     pointlist = [smt.getvectors(tile)[0] for tile in tilelist]
-    pixlist = np.array([hp.query_polygon(nside, points[:-1], nest=nest) 
+    pixlist = np.array([hp.query_polygon(nside, points[:-1], nest=True) 
                         for points in pointlist])
 
-    outfile = "{}/{}_nside{}_nest{}.pgz".format(tilesdir,scope,nside,nest)
-    with gzip.GzipFile(outfile, 'w') as f:
-        pickle.dump([tilelist,pixlist], f) #makes gzip compressed pickles
-        f.close()
+    with gzip.GzipFile(filename, 'w') as f:
+        pickle.dump([tilelist, pixlist], f) #makes gzip compressed pickles
 
-    return
-
-
-def tileallsky(tilesdir):
-
-    if not os.path.exists(tilesdir):
-        os.makedirs(tilesdir)
-
-    scopes = ['GOTO4','GOTO8']
-
-    for scope in scopes:
-        delns,delew = smt.getdels(scope)
-
-        tilelist = []
-
-        north = np.arange(0.0,90.0,delns)
-        south = -1*north
-        n2s = np.append(south[::-2],north)
-        e2w = np.arange(0.0,360.,delew)
-
-        tilelist = np.array([smt.findFoV(lon,lat,delns,delew) 
-                             for lat,lon in it.product(n2s,e2w)])
-
-        pixelsky(tilesdir,tilelist,scope)
-
-    return
 
 def readtiles(infile,metadata):
 
@@ -58,6 +39,7 @@ def readtiles(infile,metadata):
         f.close()
 
     return tilelist,pixlist
+
 
 if __name__=='__main__':
 
