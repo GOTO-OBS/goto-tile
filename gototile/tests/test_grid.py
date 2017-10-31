47294c62c19060cc157510069ddd916850087386
Evert Rol <evert.rol@gmail.com>
2017-10-31 11:31:15 +1100

    Add gridding unit test

---
+++ gototile/tests/test_grid.py
@@ -0,0 +1,21 @@
+import numpy as np
+from astropy import units
+from gototile.grid import tileallsky
+
+
+def test_gridding():
+    fov = {'ra': 4 * units.deg, 'dec': 2 * units.deg}
+
+    # 512 requires more memory, but has higher precision. Some
+    # assertions fails with lower precision, because e.g., the maximum
+    # tile comes out as containing more than 9 square degrees
+    nside = 512
+    for overlap in [0.0, 0.5, 0.8]:
+        tiles, tile_pixels, tile_centres = tileallsky(
+            fov, nside=nside, overlap=overlap)
+        tile_pixcount = np.array([len(pixlist) for pixlist in tile_pixels])
+        tile_areas = tile_pixcount/(12*nside**2)*41253
+        if nside >= 512:
+            assert np.min(tile_areas) > 7
+            assert np.max(tile_areas) < 9
+        assert np.abs(np.mean(tile_areas) - 8) < 0.1
