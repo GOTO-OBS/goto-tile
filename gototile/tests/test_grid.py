4fd602fa1d2e1edacabb352248bba69a87a04bb1
Martin Dyer <martin.dyer@sheffield.ac.uk>
2018-11-16 12:52:01 +0000

    Update test

--- gototile/tests/test_grid.py
+++ gototile/tests/test_grid.py
@@ -1,19 +1,21 @@
 import numpy as np
-from astropy import units
-from gototile.grid import tileallsky
+from astropy import units as u
+from gototile.grid import SkyGrid
 
 
 def test_gridding():
-    fov = {'ra': 4 * units.deg, 'dec': 2 * units.deg}
+    fov = {'ra': 4 * u.deg, 'dec': 2 * u.deg}
 
     # 512 requires more memory, but has higher precision. Some
     # assertions fails with lower precision, because e.g., the maximum
     # tile comes out as containing more than 9 square degrees
     nside = 512
     for overlap in [0.0, 0.5, 0.8]:
-        tiles, tile_pixels, tile_centres = tileallsky(
-            fov, nside=nside, overlap=overlap)
-        tile_pixcount = np.array([len(pixlist) for pixlist in tile_pixels])
+        grid = SkyGrid(fov, overlap=overlap)
+        grid.get_pixels(nside)
+
+        assert len(grid.pixels) == grid.ntiles
+        tile_pixcount = np.array([len(pixlist) for pixlist in grid.pixels])
         tile_areas = tile_pixcount/(12*nside**2)*41253
         if nside >= 512:
             assert np.min(tile_areas) > 7
