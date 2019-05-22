807595a451d876db469a2c45899da437203d790c
ewittmyl <physicsmyl@gmail.com>
2019-05-22 15:51:33 +1000

    add 'GLADE_PATH' for GLADE catalog

--- gototile/settings.py
+++ gototile/settings.py
@@ -8,6 +8,7 @@ NSIDE = 64
 TILESDIR = 'tiles'
 DATA_DIR = pkg_resources.resource_filename('gototile', 'data')
 GWGC_PATH = os.path.join(DATA_DIR, 'GWGC.csv')
+GLADE_PATH = os.path.join(DATA_DIR, 'GLADE.csv')
 TIMESTEP = TimeDelta(300 * units.second)
 SUNALTITUDE = -18 * units.degree
 COVERAGE = {'min': 0.05, 'max': 0.95}
