f850d559a521451a14e68aeb2f889d8fa6f5d2ee
Martin Dyer <martin.dyer@sheffield.ac.uk>
2025-02-05 15:52:04 +0000

    Remove unused code

--- gototile/catalog.py
+++ gototile/catalog.py
@@ -4,17 +4,14 @@ import os.path
 from collections import defaultdict
 import numpy as np
 import astropy.units as u
-from astropy.coordinates import SkyCoord, AltAz, Angle
-from astropy.table import Table
+from astropy.coordinates import SkyCoord, Angle
 import sys
-import requests
 from urllib.request import urlretrieve
 import pkg_resources
 import time
 import pandas as pd
 import healpy as hp
 
-from . import settings
 from . import skymaptools
 from .skymap import SkyMap
 
@@ -61,39 +58,6 @@ class download():
         df.to_csv(outfile, index=False)
 
         os.remove(out_txt)
-
-
-def map2catalog(skymap, catalog, key='weight'):
-    """Return a copy of the skymap folded with the catalog"""
-    weights = np.zeros(len(skymap.skymap))
-
-    phi = np.deg2rad(catalog['ra']%360)
-    theta = np.pi/2 - np.deg2rad(catalog['dec'])
-
-    catalogpixels = hp.ang2pix(skymap.nside, theta, phi, nest=skymap.is_nested)
-    sources = defaultdict(list)
-
-    for i, weight in enumerate(catalog[key]):
-        if weight:
-            pixel = catalogpixels[i]
-            weights[pixel] += weight
-            # Store the catalog source
-            sources[pixel].append((i, catalog['ra'][i], catalog['dec'][i]))
-    weightmap = skymap.copy()
-    weightmap.skymap = weights * weightmap.skymap
-
-    return weightmap, sources
-
-
-def visible_catalog(catalog, sidtimes, telescope):
-
-    mask = np.zeros(len(catalog), dtype=np.bool)
-    coords = SkyCoord(ra=catalog['ra']*u.deg, dec=catalog['dec']*u.deg)
-    for st in sidtimes:
-        frame = AltAz(obstime=st, location=telescope.location)
-        altaz = coords.transform_to(frame)
-        mask |= altaz.alt > telescope.min_elevation
-    return catalog[mask], np.where(mask)[0]
 
 
 def create_catalog_skymap(name, dist_mean=None, dist_err=None, key='weight',
